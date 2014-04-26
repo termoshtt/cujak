38ac866e8b7aad19faf520dfb8588be16320f3f7
Toshiki Teramura <toshiki.teramura@gmail.com>
2014-04-27 00:54:00 +0900

    Make fft2d::{r2c,c2r} inline

--- util/CUDA/cufft.hpp
+++ util/CUDA/cufft.hpp
@@ -7,7 +7,9 @@ namespace util {
 
 namespace fft2d {
 
+/** type traits for Real/Complex */
 template <typename> class traits;
+
 template <> struct traits<float> {
   typedef cufftReal Real;
   typedef cufftComplex Complex;
@@ -19,16 +21,32 @@ template <> struct traits<double> {
 
 /** Size of Complex data @f$ N_x * (N_y / 2 + 1) @f$
  *  (size of Real data is @f$ N_x * N_y @f$) */
-__host__ __device__ inline unsigned int padded_size(unsigned int Nx, unsigned int Ny) {
+inline unsigned int padded_size(unsigned int Nx, unsigned int Ny) {
   return Nx * ((Ny / 2) + 1);
 }
 
+/** one-time C2R conversion */
 template <typename T>
-__device__ void c2r(unsigned int Nx, unsigned int Ny, const typename traits<T>::Complex *,
-         typename traits<T>::Real *);
+inline void c2r(unsigned int Nx, unsigned int Ny,
+                const typename traits<T>::Complex *uf,
+                typename traits<T>::Real *u) {
+  cufftHandle plan;
+  CUFFT_EXEC(cufftPlan2d(&plan, Nx, Ny, CUFFT_C2R));
+  CUFFT_EXEC(
+      cufftExecC2R(plan, const_cast<typename traits<T>::Complex *>(uf), u));
+  CUFFT_EXEC(cufftDestroy(plan));
+}
+
+/** one-time R2C conversion */
 template <typename T>
-__device__ void r2c(unsigned int Nx, unsigned int Ny, const typename traits<T>::Real *,
-         typename traits<T>::Complex *);
+inline void r2c(unsigned int Nx, unsigned int Ny,
+                const typename traits<T>::Real *u,
+                typename traits<T>::Complex *uf) {
+  cufftHandle plan;
+  CUFFT_EXEC(cufftPlan2d(&plan, Nx, Ny, CUFFT_R2C));
+  CUFFT_EXEC(cufftExecR2C(plan, const_cast<typename traits<T>::Real *>(u), uf));
+  CUFFT_EXEC(cufftDestroy(plan));
+}
 
 } // namespace fft2d
 
