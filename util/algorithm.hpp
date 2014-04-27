3a76784ef73126069709672838dab7b34d901299
Toshiki Teramura <toshiki.teramura@gmail.com>
2014-04-28 04:18:13 +0900

    Make algorithms inline

--- util/algorithm.hpp
+++ util/algorithm.hpp
@@ -9,7 +9,7 @@ namespace util {
 
 /** square of L2-norm */
 template <class InputIterator>
-typename std::iterator_traits<InputIterator>::value_type
+inline typename std::iterator_traits<InputIterator>::value_type
 square_sum(InputIterator begin, InputIterator end) {
   typedef typename std::iterator_traits<InputIterator>::value_type T;
   return std::accumulate(begin, end, T(0), [=](T x, T y) { return x + y * y; });
@@ -17,12 +17,12 @@ square_sum(InputIterator begin, InputIterator end) {
 
 /** L2-norm */
 template <class InputIterator>
-double l2(InputIterator begin, InputIterator end) {
+inline double l2(InputIterator begin, InputIterator end) {
   return std::sqrt(square_sum(begin, end));
 }
 
 template <class InputIterator>
-typename std::iterator_traits<InputIterator>::value_type
+inline typename std::iterator_traits<InputIterator>::value_type
 l1(InputIterator begin, InputIterator end) {
   typedef typename std::iterator_traits<InputIterator>::value_type T;
   return std::accumulate(begin, end, T(0), [=](T x, T y) { return x + std::abs(y); });
