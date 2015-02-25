5f9dcfade734511e1c1e4eeae4194ffb3442d188
Toshiki Teramura <toshiki.teramura@gmail.com>
2015-02-26 02:06:39 +0900

    Add moc and document

---
+++ EOM/RK4.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "interface.hpp"
+
+namespace cujak {
+namespace EOM {
+
+/*!
+ * @class RK4
+ * @brief classical Runge-Kutta method
+ * @headerfile RK4.hpp "cujak/EOM/RK4.hpp"
+ *
+ * solve autonomous nonlinear equation
+ * @f$
+ *   \partial_t u = A u + g(u) + f
+ * @f$
+ * using classical 4th-order Runge-Kutta method.
+ *
+ * @tparam GreenFunc (Inherit VectorFunc) Green function of Linear and
+ *                                        inhomogenious term
+ * @tparam NonLinFunc (Inherit VectorFunc) Nonlinear term
+ * @tparam VectorRange thrust-compatible range
+ */
+template <class GreenFunc, class NonLinFunc,
+          class VectorRange = typename GreenFunc::VectorRange>
+class RK4 : public VectorFunc<VectorRange> {
+public:
+  RK4(GreenFunc &, NonLinFunc &);
+
+  void operator()(VectorRange &u) const;
+  void operator()(const VectorRange &, VectorRange &) const;
+  size_t size() const;
+};
+
+} // namespace EOM
+} // namespace cujak
---
+++ EOM/interface.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+namespace cujak {
+
+/** equation of motions */
+namespace EOM {
+
+/*!
+ * @class VectorFunc
+ * @headerfile interface.hpp "cujak/EOM/interface.hpp"
+ *
+ * Vector function @f$ f : \mathcal{R}^n \to \mathcal{R}^n @f$
+ *
+ * @tparam VectorRange thrust-compatible range
+ */
+template <class VectorRange> class VectorFunc {
+public:
+  typedef VectorRange VectorRange;
+  /** inplace */
+  virtual void operator()(VectorRange &) const = 0;
+  /** outplace */
+  virtual void operator()(const VectorRange &, VectorRange &) const = 0;
+  /** return @f$ n @f$ */
+  virtual size_t size() const = 0;
+};
+
+} // namespace EOM
+} // namespace cujak
