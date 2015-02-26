587ef2842151ee5bc137ba507ad138bb9d9fead5
Toshiki Teramura <toshiki.teramura@gmail.com>
2015-02-27 03:44:41 +0900

    Implement some operators

--- test/operators.cu
+++ test/operators.cu
@@ -8,20 +8,20 @@ typedef typename cujak::traits<float>::Complex Complex;
 using namespace cujak::operators;
 using namespace cujak::math;
 
-int main(int argc, char const* argv[]) {
+int main(int argc, char const *argv[]) {
   Real a = 2.0;
-  Complex c = {1,2};
+  Complex c = { 1, 2 };
   a + c;
   c + a;
   a - c;
   c - a;
-  a * c;
-  c * a;
+  a *c;
+  c *a;
 
   2 * c;
   c + c;
-  c - c; 
-  c * c;
+  c - c;
+  c *c;
 
   exp(c);
 
