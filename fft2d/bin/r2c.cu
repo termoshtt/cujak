396ad603671e3dbea9ef4c297b67cd3dd71335d2
Toshiki Teramura <toshiki.teramura@gmail.com>
2015-12-07 15:10:43 +0900

    Implement r2c

--- fft2d/bin/r2c.cu
+++ fft2d/bin/r2c.cu
@@ -4,17 +4,15 @@
 
 namespace FFT = cujak::fft2d;
 template <typename T> void work(std::string filename) {
-  const int Nx = 128;
-  const int Ny = 128;
-  // auto pb = FFT::load_field<T>(filename);
-  // FFT::Field<T> F = FFT::pb2field<T>(pb);
-  // const int Nx = F.size_x();
-  // const int Ny = F.size_y();
-  // FFT::planR2C<T> p(Nx, Ny);
-  // FFT::Coefficient<float> C(Nx, Ny);
-  // p(F, C);
-  // std::string prefix(filename.begin(), filename.end() - 3);
-  // FFT::save_pb(C, prefix);
+  auto pb = FFT::load_field<T>(filename);
+  FFT::Field<T> F = FFT::pb2field<T>(pb);
+  const int Nx = F.size_x();
+  const int Ny = F.size_y();
+  FFT::planR2C<T> p(Nx, Ny);
+  FFT::Coefficient<T> C(Nx, Ny);
+  p(F, C);
+  std::string prefix(filename.begin(), filename.end() - 3);
+  FFT::save_pb(C, prefix);
 }
 
 int main(int argc, char const *argv[]) {
