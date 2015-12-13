092022f359918c2967f22ca45680f95d623b37f3
Toshiki Teramura <toshiki.teramura@gmail.com>
2015-12-13 17:30:31 +0900

    Make operators inline

--- operators.hpp
+++ operators.hpp
@@ -8,43 +8,51 @@ namespace cujak {
 namespace operators {
 
 #define cujakDefCPlus(C)                                                       \
-  __host__ __device__ C operator+(C c, C d) {                                  \
+  __host__ __device__ inline C operator+(C c, C d) {                           \
     return complex(c.x + d.x, c.y + d.y);                                      \
   }
 cujakDefCPlus(cuComplex);
 cujakDefCPlus(cuDoubleComplex);
 
 #define cujakDefCMinus(C)                                                      \
-  __host__ __device__ C operator-(C c, C d) {                                  \
+  __host__ __device__ inline C operator-(C c, C d) {                           \
     return complex(c.x - d.x, c.y - d.y);                                      \
   }
 cujakDefCMinus(cuComplex);
 cujakDefCMinus(cuDoubleComplex);
 
 #define cujakDefCMult(C)                                                       \
-  __host__ __device__ C operator*(C c, C d) {                                  \
+  __host__ __device__ inline C operator*(C c, C d) {                           \
     return complex(c.x * d.x - c.y * d.y, c.y * d.x + c.x * d.y);              \
   }
 cujakDefCMult(cuComplex);
 cujakDefCMult(cuDoubleComplex);
 
 #define cujakDefPlus(R, C)                                                     \
-  __host__ __device__ C operator+(R r, C c) { return complex(r + c.x, c.y); }  \
-  __host__ __device__ C operator+(C c, R r) { return complex(r + c.x, c.y); }
+  __host__ __device__ inline C operator+(R r, C c) {                           \
+    return complex(r + c.x, c.y);                                              \
+  }                                                                            \
+  __host__ __device__ inline C operator+(C c, R r) {                           \
+    return complex(r + c.x, c.y);                                              \
+  }
 cujakDefPlus(float, cuComplex);
 cujakDefPlus(double, cuDoubleComplex);
 
 #define cujakDefMinus(R, C)                                                    \
-  __host__ __device__ C operator-(R r, C c) { return complex(r - c.x, c.y); }  \
-  __host__ __device__ C operator-(C c, R r) { return complex(c.x - r, c.y); }
+  __host__ __device__ inline C operator-(R r, C c) {                           \
+    return complex(r - c.x, c.y);                                              \
+  }                                                                            \
+  __host__ __device__ inline C operator-(C c, R r) {                           \
+    return complex(c.x - r, c.y);                                              \
+  }
 cujakDefMinus(float, cuComplex);
 cujakDefMinus(double, cuDoubleComplex);
 
 #define cujakDefMult(R, C)                                                     \
-  __host__ __device__ C operator*(R r, C c) {                                  \
+  __host__ __device__ inline C operator*(R r, C c) {                           \
     return complex(r * c.x, r * c.y);                                          \
   }                                                                            \
-  __host__ __device__ C operator*(C c, R r) {                                  \
+  __host__ __device__ inline C operator*(C c, R r) {                           \
     return complex(r * c.x, r * c.y);                                          \
   }
 cujakDefMult(float, cuComplex);
